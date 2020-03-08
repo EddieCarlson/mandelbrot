4df0afdf5e08e9ba9b11604409fe0a541339a8dd
Eddie Carlson <eddie@eclap.local>
2020-03-08 16:21:13 -0700

    cleanup

--- src/main/scala/eddie/Complex.scala
+++ src/main/scala/eddie/Complex.scala
@@ -1,12 +1,7 @@
 package eddie
 
-import java.awt.Component
-import java.awt.Color
-import java.awt.Graphics
-import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 import java.io.File
-import java.util.Random
 
 import javax.imageio.ImageIO
 
@@ -47,49 +42,8 @@ class Grid(val rPixels: Int, rMin: Double = -2, rMax: Double = 1, iMin: Double =
 
   val gridNums = grid.map(_.map(_._1))
 
-//  grid.map(x => println(x.map { case (c, _) => toChar(c, Mandelbrot.bounded)}.mkString("")))
-//  grid.map(x => println(x.map { case (_, ee) => ee.toString}.mkString("")))
-//
-//  def toChar(c: Complex, f: (Complex, Complex, Int) => Boolean) = {
-//    if (f(c, Complex.zero, 0)) {
-//      "x"
-//    } else {
-//      " "
-//    }
-//  }
-
   private def pixels(size: Double, offset: Double): Seq[Double] =
     1.to((size * pixelsPerUnit).toInt).map(d => (d.toDouble / pixelsPerUnit) + offset)
-
-//  def toImage2(f: Complex => Boolean): Image = {
-//    grid.map { rows =>
-//      rows.map { c =>
-//        val color = if (f(c)) Color.purple else Color.lightBlue
-//        Image.rectangle(5, 5).fillColor(color)
-//      }.foldLeft(Image.empty)(_.beside(_))
-//    }.foldLeft(Image.empty)(_.above(_))
-//  }
-
-//  def escapeBoundIterCounts(f: Complex => Int): Map[Int, Double] = {
-//    grid.flatMap { rows => rows.map(f) }.groupBy(identity).mapValues(_.size / totalPixels)
-//  }
-
-//  def toImage(f: Complex => Boolean): Image = {
-//    grid.foldLeft(Empty: Image) { (i, row) =>
-//      println("a")
-//      i.above {
-//        row.foldLeft(i) { (iRow, c) =>
-//          println(".")
-//          val color = if (f(c)) Color.black else Color.white
-//          val image = Image.rectangle(30, 30).fillColor(color)
-//          iRow.beside(image)
-//        }
-//      }
-//    }
-//  }
-
-//  def display(f: Complex => Boolean) =
-//    grid.map(_.map(c => if (f(c)) "x" else " ")).foreach(d => println(d.mkString))
 }
 
 object MyColors {
@@ -140,26 +94,13 @@ object Mandelbrot extends App {
   val personalLaptopDir = "/Users/eddie/IdeaProjects/mandelbrot"
 
   println("creating grid")
-  val g = new Grid(500)
+  val g = new Grid(5000)
 
   val img = new BufferedImage(g.rPixels, g.iPixels, BufferedImage.TYPE_INT_ARGB)
 
   def toColor(c: Complex) = {
     val count = boundedCount(c)
-
-    val cc = MyColors.chooseColor(count)
-//    println(cc)
-    cc
-
-//    if (count == 1000) Color.CYAN
-//    else if (count > 400) Color.RED
-//    else if (count > 100) Color.ORANGE
-//    else if (count > 70) Color.YELLOW
-//    else if (count > 25) Color.GREEN
-//    else if (count > 15) Color.LIGHT_GRAY
-//    else if (count > 10) Color.GRAY
-//    else if (count > 5) Color.DARK_GRAY
-//    else Color.BLACK
+    MyColors.chooseColor(count)
   }
 
   println("converting to colors")
@@ -172,22 +113,7 @@ object Mandelbrot extends App {
     }
   }
 
-//  val width = 60
-//  val height = 40
-//  val canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
-//
-//  0.until(width).foreach { w => 0.until(height).foreach { h => canvas.setRGB(w, h, Color.WHITE.getRGB) } }
-//
-//  canvas.setRGB(3, 10, Color.BLACK.getRGB)
-
   println("writing file")
   val outputFile = new File(s"$personalLaptopDir/testAwtMandelbrot4.png")
   ImageIO.write(img, "png", outputFile)
-
-//  val i = new Grid(600).toImage2(bounded(_))
-//  println(new Grid(600).escapeBoundIterCounts(boundedCount(_)).toList.sortBy(_._2).reverse)
-//  println("out")
-//  i.write[Png]("/Users/eddie.carlson/developer/eddie/mandelbrot/test6.png")
-//  new Grid(500, -.8, -.7, .1, .2).display(bounded(_))
-//  new Grid(500, -.75, -.725, .125, .175).display(bounded(_))
 }
