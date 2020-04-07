8fc27c1c7e9523be97c9fefdf700270345dff4b4
Eddie Carlson <eddie@eclap.local>
2020-04-06 21:30:39 -0700

    it lives!

--- src/main/scala/eddie/Mandelbrot.scala
+++ src/main/scala/eddie/Mandelbrot.scala
@@ -33,7 +33,8 @@ object Mandelbrot extends App {
 
 //  val g = new Grid(rPixels = 120, rMin = 1.16 - 2, rMax = 1.30 - 2, iMin = 1.0356 - 1, iMax = 1.259 - 1)
 //    val g = new Grid(rPixels = 15000, rMin = -0.6704, rMax = -0.41495, iMin = 0.5063, iMax = 0.7196)
-  val g = Grid(rPixels = 150)
+  val g = new Grid(rPixels = 1300, rMin = -1.0704, rMax = -0.41495, iMin = 0.4063, iMax = 0.8596)
+//  val g = Grid(rPixels = 1300)
 
   val mirrorGate = true
   val mirror = mirrorGate && g.iMin == 0
@@ -48,8 +49,17 @@ object Mandelbrot extends App {
     count.map(MyColors.chooseColor).getOrElse(MyColors.blue.getRGB)
   }
 
+  def toBound(c: Complex): Int = {
+    val count = boundedCount(c)
+    count.getOrElse(1000)
+  }
+
   def cGrid = g.gridPixels.zipWithIndex.map { case (row, h) =>
     (row.map(toColor).zipWithIndex, h)
+  }
+
+  def bGrid = g.gridPixels.zipWithIndex.map { case (row, h) =>
+    (row.map(toBound).zipWithIndex, h)
   }
 
   val setRGB = if (mirror) {
@@ -68,11 +78,18 @@ object Mandelbrot extends App {
     }
   }
 
+  val pixelGroups = bGrid.flatMap { case (row, h) =>
+    if (h % 1000 == 0) println(h)
+    row.map { case (bound, w) =>
+      (bound, (w, h))
+    }
+  }.groupBy(_._1).mapValues(_.map(_._2).toSet).toList.sortBy(_._1)
+
 
   println()
   println(s"took: ${(System.currentTimeMillis - start) / 1000} seconds")
 
-  val tenColorImage = new BufferedImage(50, 500, BufferedImage.TYPE_INT_ARGB)
+//  val tenColorImage = new BufferedImage(50, 500, BufferedImage.TYPE_INT_ARGB)
   val colors = List(
     "#002222",
     "#003333",
@@ -86,31 +103,68 @@ object Mandelbrot extends App {
     "#00BBBB"
   ).map(Color.decode).map(_.getRGB)
 
-  val pixelMap = colors.zipWithIndex.flatMap { case (color, imgNum) =>
-    0.to(49).flatMap { x =>
-      0.to(49).map { internalY =>
-        val y = internalY + (imgNum * 50)
-        tenColorImage.setRGB(x, y, color)
-        (imgNum, (x, y))
-      }
+  val thresholds = List(
+    3,
+    5,
+    8,
+    11,
+    15,
+    19,
+    24,
+    29,
+    35,
+    45,
+    51,
+    57,
+    65,
+    75,
+    90,
+    110,
+    140,
+    180,
+    250,
+    999,
+    1000
+  )
+  def partitionSets(remainingThresh: List[Int], remainingPixelSets: List[(Int, Set[(Int, Int)])], acc: List[Set[(Int, Int)]] = Nil): List[Set[(Int, Int)]] = {
+    remainingThresh match {
+      case Nil => acc
+      case h :: t =>
+        val (inSet, notInSet) = remainingPixelSets.partition(_._1 <= h)
+        val newAcc = inSet.flatMap(_._2).toSet :: acc
+        partitionSets(t, notInSet, newAcc)
     }
-  }.groupBy(_._1).mapValues(_.map(_._2))
+  }
 
-  val icon = new ImageIcon(tenColorImage)
+  val pixelSets = partitionSets(thresholds, pixelGroups).reverse
+
+
+//  val pixelMap = colors.zipWithIndex.flatMap { case (color, imgNum) =>
+//    0.to(49).flatMap { x =>
+//      0.to(49).map { internalY =>
+//        val y = internalY + (imgNum * 50)
+//        tenColorImage.setRGB(x, y, color)
+//        (imgNum, (x, y))
+//      }
+//    }
+//  }.groupBy(_._1).mapValues(_.map(_._2))
+
+  val icon = new ImageIcon(img)
   val frame = new JFrame
 
   val colorPanel = new JPanel()
-  colorPanel.setLayout(new GridLayout(10, 2))
-  val components = 0.to(9).map(i => new JButton("set color") -> (new JTextField(6), i))
-  components.foreach { case (butt, (txt, _)) => colorPanel.add(txt); colorPanel.add(butt) }
-  val buttMap = components.toMap.mapValues { case (txt, i) => (txt, pixelMap(i)) }
-  buttMap.values.foreach { case (txt, (x, y) :: _) => txt.setText(tenColorImage.getRGB(x, y).toHexString.substring(2)) }
+  colorPanel.setLayout(new GridLayout(thresholds.size, 3))
+  val components = thresholds.indices.map(i => new JButton("set color") -> (new JTextField(6), i))
+  val thresholdLabels = thresholds.map(t => new JLabel(s"- to $t"))
+  components.zip(thresholdLabels).foreach { case ((butt, (txt, _)), thresh) => colorPanel.add(thresh); colorPanel.add(txt); colorPanel.add(butt) }
+  val buttMap = components.toMap.mapValues { case (txt, i) => (txt, pixelSets(i).toList) }
+  buttMap.values.foreach { case (txt, (x, y) :: _) => txt.setText(img.getRGB(x, y).toHexString.substring(2)) }
   val lbl = new JLabel
   buttMap.foreach { case (butt, (txt, pixels)) =>
-    butt.addActionListener(new ButtonListener(pixels, tenColorImage, lbl, txt))
+    butt.addActionListener(new ButtonListener(pixels, img, lbl, txt))
   }
   frame.setLayout(new FlowLayout)
-  frame.setSize(1600, 1000)
+  frame.setSize(4000, 2500)
   lbl.setIcon(icon)
   frame.add(lbl)
   frame.add(colorPanel)
